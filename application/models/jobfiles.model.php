4ddc258f8dd8130f05fab43676fac3bd1ba006d4
Tom Hodder <tom@limepepper.co.uk>
2021-09-19 04:53:36 +0100

    initial attempt at fixing the sql for change to Filename table

--- application/models/jobfiles.model.php
+++ application/models/jobfiles.model.php
@@ -9,28 +9,68 @@
  */
 
 class JobFiles_Model extends CModel {
-	
+
+  protected $dbVersionId;
+
+  protected function getDbVersionId(){
+      if($this->dbVersionId){
+          return $this->dbVersionId;
+      }
+      $sqlQuery = CDBQuery::get_Select( array('table' => 'Version',
+          'fields' => array('VersionId'),
+          'limit' => array( 'count' => 1)
+        ), $this->driver );
+      $result = $this->run_query($sqlQuery);
+      $dbVersionId = $result->fetchColumn();
+      if($dbVersionId){
+          $this->dbVersionId = $dbVersionId;
+          return $this->dbVersionId;
+      }
+  }
+
 	public function getJobFiles($jobId, $limit, $offset, $filename = ''){
         $used_types = array();
 
-        $fields = array('Job.Name', 'Job.JobStatus', 'File.FileIndex', 'Path.Path', 'Filename.Name');
-        $where = array("File.JobId = $jobId");
+          if($this->getDbVersionId()<1016){
+              $fields = array('Job.Name', 'Job.JobStatus', 'File.FileIndex', 'Path.Path', 'Filename.Name');
 
-        if( !empty($filename) ) {
-           $where[] = "(Filename.Name LIKE '%$filename%' OR Path.Path LIKE '%$filename%' OR concat(Path.Path, '', Filename.Name) = '$filename')";
+              $where = array("File.JobId = $jobId");
+
+              if( !empty($filename) ) {
+                $where[] = "(Filename.Name LIKE '%$filename%' OR Path.Path LIKE '%$filename%' OR concat(Path.Path, '', Filename.Name) = '$filename')";
+              }
+
+              $orderby = 'File.FileIndex ASC';
+              $sqlQuery = CDBQuery::get_Select( array('table' => 'File',
+                  'fields' => $fields,
+                  'join' =>   array(  array('table' => 'Path', 'condition' => 'Path.PathId = File.PathId'),
+                                      array('table' => 'Filename', 'condition' => 'File.FilenameId = Filename.FilenameId'),
+                                      array('table' => 'Job', 'condition' => 'Job.JobId = File.JobId') ),
+                  'where' => $where,
+                  'orderby' => $orderby,
+                  'limit' => array( 'count' => $limit, 'offset' => $offset*$limit),
+                  'offset' => ($offset*$limit)
+              ), $this->driver );
+
+          }else{
+              $fields = array('Job.Name', 'Job.JobStatus', 'File.FileIndex', 'Path.Path', 'File.Filename');
+              $where = array("File.JobId = $jobId");
+
+              if( !empty($filename) ) {
+                $where[] = "(File.Filename LIKE '%$filename%' OR Path.Path LIKE '%$filename%' OR concat(Path.Path, '', File.Filename) = '$filename')";
+              }
+
+              $orderby = 'File.FileIndex ASC';
+              $sqlQuery = CDBQuery::get_Select( array('table' => 'File',
+                  'fields' => $fields,
+                  'join' =>   array(  array('table' => 'Path', 'condition' => 'Path.PathId = File.PathId'),
+                                      array('table' => 'Job', 'condition' => 'Job.JobId = File.JobId') ),
+                  'where' => $where,
+                  'orderby' => $orderby,
+                  'limit' => array( 'count' => $limit, 'offset' => $offset*$limit),
+                  'offset' => ($offset*$limit)
+              ), $this->driver );
         }
-        
-        $orderby = 'File.FileIndex ASC';
-        $sqlQuery = CDBQuery::get_Select( array('table' => 'File',
-            'fields' => $fields,
-            'join' =>   array(  array('table' => 'Path', 'condition' => 'Path.PathId = File.PathId'),
-                                array('table' => 'Filename', 'condition' => 'File.FilenameId = Filename.FilenameId'),
-                                array('table' => 'Job', 'condition' => 'Job.JobId = File.JobId') ),
-            'where' => $where,
-            'orderby' => $orderby,
-            'limit' => array( 'count' => $limit, 'offset' => $offset*$limit), 
-            'offset' => ($offset*$limit)
-         ), $this->driver );
 
         $result = $this->run_query($sqlQuery);
 
@@ -38,20 +78,35 @@ class JobFiles_Model extends CModel {
 
         return $used_types;
 	}
-	
+
 	public function countJobFiles($jobId, $filename = ''){
 		$used_types = array();
-        $sql_query = "SELECT COUNT(*) as count
-			FROM File, Path, Filename, Job 
-			WHERE File.JobId = $jobId 
-			AND  Path.PathId = File.PathId 
-			AND  Filename.FilenameId = File.FilenameId 
-            AND  Job.JobId = File.JobId ";
 
-      // Filter by filename if requested
-      if( !empty($filename)) {
-         $sql_query .= "AND (Filename.Name LIKE '%$filename%' OR Path.Path LIKE '%$filename%' OR concat(Path.Path, '', Filename.Name) = '$filename') ";
-      }
+
+    if($this->getDbVersionId()<1016){
+          $sql_query = "SELECT COUNT(*) as count
+        FROM File, Path, Filename, Job
+        WHERE File.JobId = $jobId
+        AND  Path.PathId = File.PathId
+        AND  Filename.FilenameId = File.FilenameId
+              AND  Job.JobId = File.JobId ";
+
+        // Filter by filename if requested
+        if( !empty($filename)) {
+          $sql_query .= "AND (Filename.Name LIKE '%$filename%' OR Path.Path LIKE '%$filename%' OR concat(Path.Path, '', Filename.Name) = '$filename') ";
+        }
+    } else {
+          $sql_query = "SELECT COUNT(*) as count
+        FROM File, Path, Job
+        WHERE File.JobId = $jobId
+        AND  Path.PathId = File.PathId
+              AND  Job.JobId = File.JobId ";
+
+        // Filter by filename if requested
+        if( !empty($filename)) {
+          $sql_query .= "AND (File.Filename LIKE '%$filename%' OR Path.Path LIKE '%$filename%' OR concat(Path.Path, '', File.Filename) = '$filename') ";
+        }
+    }
 
       $sql_query .= ";";
 
@@ -61,11 +116,11 @@ class JobFiles_Model extends CModel {
 
       return $used_types[0]['count'];
 	}
-	
+
 	public function getJobNameAndJobStatusByJobId($jobId){
 		$used_types = array();
         $sql_query = "SELECT distinct Job.Name, Job.JobStatus
-			FROM Job 
+			FROM Job
 			WHERE Job.JobId = $jobId;";
         $result = $this->run_query($sql_query);
 
@@ -107,7 +162,7 @@ class JobFiles_Model extends CModel {
 					break;
 			}
 		}
-		
+
         return $used_types;
 	}
 }
